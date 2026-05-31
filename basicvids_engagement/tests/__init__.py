ddd920afa67eac84e6b169db8d92641c0d7fb988
ilyabasicboy <ilya951.com@gmail.com>
2026-05-31 22:01:39 +0500

    Add GitHub Actions CI

--- basicvids_engagement/tests/__init__.py
+++ basicvids_engagement/tests/__init__.py
@@ -1,3 +1,11 @@
+import os
+import tempfile
+
+
+test_data_path = tempfile.mkdtemp(prefix="basicvids_engagement_tests_")
+os.environ["DATA_PATH"] = test_data_path
+os.environ["DATABASE_URL"] = f"sqlite:///{test_data_path}/database.db"
+
 from basicvids_engagement.db import create_db_and_tables, engine
 from basicvids_engagement.main import app
 
